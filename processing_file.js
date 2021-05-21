74bada39fbaf5a653022cc4c371386f8ba03e7c4
Alex Tyson <atyson@bowdoin.edu>
2021-05-20 22:03:05 -0400

    Update processing_file.js

--- processing_file.js
+++ processing_file.js
@@ -25,50 +25,48 @@ function draw() {
     }
   }
 
-let Particle = function(x, y) {
-  // location = createVector(x, y);
-  randDegrees = random(360)
-  vel = createVector(cos(radians(randDegrees), sin(radians(randDegrees))));
+class Particle {
+  constructor(x, y) {
+    // location = createVector(x, y);
+    var randDegrees = random(360);
+    var vel = createVector(cos(radians(randDegrees), sin(radians(randDegrees))));
 
-  this.acceleration = createVector(random(0, 0.05), random(0, 0.05));
-  this.velocity = vel.mult(random(5));
-  this.lifespan = random(50, 70);
-  this.color = color(random(255), random(255), random(255));
-  this.weightRange = random(3,50);
-  this.loc = createVector(x,y);
-  this.x = x;
-  this.y = y;
-  };
+    this.acceleration = createVector(0, random(0.1));
+    this.velocity = vel.mult(random(5));
+    this.lifespan = random(50, 70);
+    this.color = color(random(255), random(255), random(255));
+    this.weightRange = random(3, 50);
+    this.loc = createVector(x, y);
+    this.x = x;
+    this.y = y;
+  }
+  run() {
+    this.update();
+    this.display();
+  }
+  // Method to update position
+  update() {
+    var noiseScale = 0.2;
+    var noiseValue = (noise((this.loc.x) * noiseScale, (this.loc.y) * noiseScale) * (2 * PI) * 4);
 
-Particle.prototype.run = function() {
-  this.update();
-  this.display();
-  };
+    var dir = createVector(cos(noiseValue), sin(noiseValue));
 
-// Method to update position
-Particle.prototype.update = function(){
-  noiseScale = 0.2;
-  noiseValue = (noise((this.loc.x) * noiseScale, (this.loc.y) * noiseScale) * 4 * PI);
+    this.velocity.add(dir);
+    this.loc.add(this.velocity);
+    this.lifespan -= 2;
+  }
+  // Method to display
+  display() {
+    strokeWeight(this.lifespan + 1.5);
+    stroke(0, (this.lifespan * 120));
+    point(this.loc.x, this.loc.y);
 
-  dir = createVector(cos(noiseValue), sin(noiseValue));
-
-  this.velocity.add(dir);
-  this.loc.add(this.velocity);
-  this.lifespan -= 2;
-  };
-
-// Method to display
-Particle.prototype.display = function() {
-  strokeWeight(this.lifespan + 1.5);
-  stroke(0, (this.lifespan * 120));
-  point(this.loc.x, this.loc.y);
-
-  strokeWeight(this.lifespan);
-  stroke(this.color);
-  point(this.loc.x, this.loc.y);
-  };
-
-// Is the particle still useful?
-Particle.prototype.isDead = function(){
-  return this.lifespan < 0;
-  };
+    strokeWeight(this.lifespan);
+    stroke(this.color);
+    point(this.loc.x, this.loc.y);
+  }
+  // Is the particle still useful?
+  isDead() {
+    return this.lifespan < 0;
+  }
+}
